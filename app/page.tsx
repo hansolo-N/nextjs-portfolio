603f4b26bbdc0eff813aa844599840013012f43d
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-12-12 19:36:28 +0200

    added progressbar to main page based on scroll y value on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react'
 import HeroLayout from './components/HeroLayout'
 import Projects from './components/Projects'
+import styles from './components/page.module.css'
+import { motion,useMotionValue,useMotionValueEvent, useScroll } from "framer-motion"
 
 export default function Home() {
+  const [progress,setProgress] = useState(0)
+  const progressValue = useMotionValue(0);
+  const { scrollY ,scrollYProgress} = useScroll()
+
+  const current = scrollYProgress.get()
+
+
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setProgress((latest/1000))
+  })
+
+
   return (
-    <div className='flex flex-col gap-6'>
+    <motion.div className='flex flex-col gap-6'>
+    {progress > 0 && <motion.progress value={progress} max={1} className={styles.progressbar}>progress bar</motion.progress>}
     <HeroLayout/>
     <Projects/>
-    </div>
+    </motion.div>
+      
+
     )
 }
