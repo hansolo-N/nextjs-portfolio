8ee401972e734cfd95db08d8eae75dbccb6ff268
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-12-12 16:41:26 +0200

    updated project card components and global styling

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -13,19 +13,21 @@ import Link from "next/link"
 interface Props{
   cardTitle : string,
   imgSrc: string
+  cardContent : string
 }
 
 
 
-function ProjectCard({cardTitle,imgSrc}:Props): JSX.Element {
+function ProjectCard({cardTitle,imgSrc,cardContent}:Props): JSX.Element {
   return (
     <Card className={styles.card}>
-        <Image className={styles.cardImg} src={`/${imgSrc}`} alt="image not found" width={100} height={100}/>
-  <CardHeader>
-    <CardTitle>{cardTitle}</CardTitle>
+        {/* <Image className={styles.cardImg} src={`/${imgSrc}`} alt="image not found" width={100} height={100}/> */}
+        <div className={styles.test}></div>
+  <CardHeader >
+    <CardTitle className={styles[`card-header`]}>{cardTitle}</CardTitle>
   </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
+  <CardContent className={styles[`card-content`]}>
+    {cardContent}
   </CardContent>
   <CardFooter className={styles[`btn-container`]}>
   <Link href='' className={`${buttonVariants({ variant: "outline" })} ${styles.btn}`}>Live Demo</Link>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProjectCard from './ProjectCard'
+import styles from './Projects.module.css'
 import {
   CardContent,
 
@@ -7,9 +8,15 @@ import {
 
 function Projects() {
   return (
-    <div className='flex gap-4'>
-        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg'/>
-        <ProjectCard cardTitle='project2' imgSrc='shibuya.jpg'/>
+    <div className={`flex gap-4 ${styles.projectsContainer}`}>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+        <ProjectCard cardTitle='project1' imgSrc='shibuya.jpg' cardContent='this is an updated version of my portfolio using Next.Js with typescript and some other react libraries'/>
+
     </div>
   )
 }
