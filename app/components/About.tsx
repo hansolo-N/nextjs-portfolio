13d0311fd174818104103877b0b158fea5b5a672
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-12-13 13:05:02 +0200

    added interface for achievement component props,fixed icon element not loading

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import styles from './About.module.css'
 import Image from 'next/image'
+import Achievement from './Achievement'
+import { HiAcademicCap } from "react-icons/hi2";
+import { RiBug2Line } from "react-icons/ri";
 
 function About() {
   return (
     <div className={styles['grid-container']}>
-      <div className={`grid grid-cols-[1fr,2fr]`}>
-      <Image src="/shibuya.jpg" alt='' height={300} width={300}/>
-        <div className='grid grid-cols-2 grid-rows-2'>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel a officia provident beatae fugit, esse eius officiis debitis dignissimos libero deleniti accusantium dicta error quasi magni alias aliquid, dolorem inventore?
-          </p>
-        </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero earum quibusdam repudiandae reiciendis iste, alias eius id dolore mollitia aut voluptate in ea cum, dolorum dolor atque quasi eligendi.</p>
-      
-      <p className='col-span-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aliquam excepturi facilis deserunt suscipit in ipsam quisquam, ut laboriosam repudiandae! Aperiam dolore nam, atque quam tempore itaque quidem optio perspiciatis.</p>
+      <div className={`grid grid-cols-[1fr,1fr] `}>
+      <Image className= {`${styles['about-img']}`} src="/shibuya.jpg" alt='' height={300} width={300}/>
+
+        <div className='grid grid-cols-[1fr,1fr] grid-rows-2 gap-4'>
+          <Achievement icon={<HiAcademicCap/>} p1='B.Sc. Computer Science' p2='Information Systems' header='Education'/>
+          <Achievement icon={<RiBug2Line/>} p1='1 year' p2='hello world!' header='Experience'/>
+          <p className='col-span-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aliquam excepturi facilis deserunt suscipit in ipsam quisquam, ut laboriosam repudiandae! Aperiam dolore nam, atque quam tempore itaque quidem optio perspiciatis.</p>
       </div>
   
     </div>
--- app/components/Achievement.tsx
+++ app/components/Achievement.tsx
@@ -1,18 +1,53 @@
 'use client'
 import styled from 'styled-components'
-import React from 'react'
+import React,{ ReactNode } from 'react'
 
 interface Props{
     header: string,
-    icon: Function,
+    icon?: JSX.Element,
+    p1?: string,
+    p2?: string
 
 }
 
 
+const StyledAchievement = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 10px;
+    height: 180px;
+    width: 400px;
+    margin: 0;
+    line-height: 2;
+`
 
-function Achievement() {
+const Header = styled.h1`
+    font-size: 18px;
+    font-weight: 800 ;
+    color: black;
+
+`
+
+const Paragraph = styled.p`
+    font-size: medium;
+`
+const Icon = styled.span`
+    font-size: 36px;
+    color: #a5bdfd;
+`
+
+
+function Achievement({header,icon,p1,p2}:Props){
   return (
-    <div>Achievement</div>
+    <StyledAchievement>
+        <Icon>{icon}</Icon>
+        <Header>{header}</Header>
+        <Paragraph>{p1}</Paragraph>
+        <Paragraph>{p2}</Paragraph>
+    </StyledAchievement>
   )
 }
 
