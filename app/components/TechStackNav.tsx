7b6fbab351d0aee87c9b9ad8741f0c9a5db20dc8
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-12-14 18:55:22 +0200

    redid tech stack carousel using swiper lirabrary for react

--- app/components/TechStackNav.tsx
+++ app/components/TechStackNav.tsx
@@ -1,6 +1,16 @@
+'use client'
 import React, { useState } from 'react'
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination'
+import 'swiper/css/navigation';
+import { EffectCoverflow,Pagination,Navigation } from 'swiper/modules';
 import styles from './TechStack.module.css'
 import icons from '../data/Icons'
+import { Button } from "@/components/ui/button"
+import { IoChevronBackSharp } from "react-icons/io5";
+import { IoChevronForward } from "react-icons/io5";
 
 function TechStackNav() {
     const [index,setIndex] = useState(0)
@@ -19,18 +29,57 @@ function TechStackNav() {
 
 
   return (
-    <div className={`${styles['stack-container']}`}>
-        <button className={styles.btn} onClick={handleDecreaseClick}>-</button>
-        <div className={styles.icons}>
-        <p><i className={`ci ${icons.at(index-1)?.name} ci-3x`}></i></p>
-        <p><i className={`ci ${icons.at(index)?.name} ci-3x`}></i></p>
-        <p><i className={`ci ${icons.at(index+1)?.name} ci-3x`}></i></p>
+    <div className={`${styles['gallery']}`}>
+        <div >
+        { icons && <div >
+        <Swiper className={`${styles['swiper-container']}`} effect={'coverflow'} grabCursor={true} centeredSlides={true}  slidesPerView={3} 
+        coverflowEffect={
+          {
+            rotate:0,
+            stretch:0,
+            depth:100,
+            modifier: 2.5
+          }}
+          spaceBetween={50}
+          pagination={{el:'',clickable:true}}
+          navigation={{
+            nextEl: `${styles['swiper-btn-next']}`,
+            prevEl: `${styles['swiper-btn-prev']}`,
+          }}
+          modules={[EffectCoverflow,Pagination,Navigation]}
+          >
+          <div className={styles.slides}>
+          <SwiperSlide>
+            {({isPrev})=>
+          <p className={`${styles.icon}`}><i className={`ci ${icons.at(index-1)?.name} ci-4x ${isPrev? `${styles['prev-icon']}`: ''}`}></i></p>
+          }
+          </SwiperSlide>
+          <SwiperSlide>
+          {({isActive})=>
+          <p className={`${styles.icon}`}><i className={`ci ${icons.at(index)?.name} ci-4x ${isActive? `${styles['active-icon']}`: ''}`}></i></p>
+          }
+          </SwiperSlide>
+          <SwiperSlide>
+            {({isNext})=>
+          <p className={`${styles.icon}`}><i className={`ci ${icons.at(index+1)?.name} ci-4x ${isNext? `${styles['next-icon']}`: ''} `}></i></p>
+        }
+          </SwiperSlide>
+
+          </div>
+         
+          <div className={`${styles[`swiper-controller`]}`}>
+            <Button  className= {`${styles['swiper-btn-prev']}`} ><IoChevronBackSharp/></Button>
+            <Button className= {`${styles['swiper-btn-next']}`} ><IoChevronForward/></Button>
+          </div>
+          <div className={`${styles.pagination}`}>background</div>
+        </Swiper>
         </div>
-        <button className={styles.btn} onClick={handleIncreaseClick}>+</button>
+        }
+        </div>
+        
+       
     </div>
   )
 }
 
 export default TechStackNav
-
-//0
